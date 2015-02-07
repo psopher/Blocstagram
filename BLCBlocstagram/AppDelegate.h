c4796a71d5cc2f9af67ced5777dabefcc326f2ab
Philip Sopher <phsophe@gmail.com>
2015-02-07 08:49:13 -0500

    Initial Commit

---
+++ BLCBlocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLCBlocstagram
+//
+//  Created by Philip Sopher on 2/7/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLCBlocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLCBlocstagram
+//
+//  Created by Philip Sopher on 2/7/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
